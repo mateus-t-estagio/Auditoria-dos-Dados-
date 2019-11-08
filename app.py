a183403b47f7b8e70cda6e347da5cc703188a351
mateus-t-estagio <57234792+mateus-t-estagio@users.noreply.github.com>
2019-11-08 16:01:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
-import 
+import plotly
 #import plotly.graph_objs as go
 #from plotly.subplots import make_subplots
 from dash.dependencies import Input, Output
